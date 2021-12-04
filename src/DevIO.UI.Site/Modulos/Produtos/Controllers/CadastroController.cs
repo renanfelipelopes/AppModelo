49aca5a96e84d5df2dd7d9571f5f2c79d7ecd49c
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-04 15:24:49 -0300

    AreaRoutes

--- src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
+++ src/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DevIO.UI.Site.Areas.Produtos.Controllers
+namespace DevIO.UI.Site.Modulos.Produtos.Controllers
 {
     [Area("Produtos")]
+    [Route("produtos")]
     public class CadastroController : Controller
     {
+        [Route("lista")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("busca")]
+        public IActionResult Busca()
         {
             return View();
         }
---
+++ src/DevIO.UI.Site/Modulos/Vendas/Controllers/PedidosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Modulos.Vendas.Controllers
+{
+    [Area("Vendas")]
+    public class PedidosController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
