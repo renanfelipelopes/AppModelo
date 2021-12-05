442a06c50ed0be02171be98067e37154e4dbe210
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-05 18:10:11 -0300

    Configurando o DbContext

---
+++ src/DevIO.UI.Site/Data/MeuDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.UI.Site.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options)
+        : base(options)
+        {
+
+        }
+    }
+}
