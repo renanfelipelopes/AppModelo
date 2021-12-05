2896ba84365d52d98fbe377ba08c0eadef9c3778
Renan Lopes <renanlopes.ads@gmail.com>
2021-12-04 21:41:13 -0300

    dependency injection

--- src/DevIO.UI.Site/Controllers/HomeController.cs
+++ src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.UI.Site.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public IActionResult Index()
         {
+            var pedido = _pedidoRepository.ObterPedido();
+
             return View();
         }
     }
